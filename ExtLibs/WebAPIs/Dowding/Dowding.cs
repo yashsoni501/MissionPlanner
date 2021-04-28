d963bbeaac91950cc5d9801dad3592c8b3119ee1
Michael Oborne <mich146@hotmail.com>
2021-04-28 20:07:39 +1000

    WebAPI: update

--- ExtLibs/WebAPIs/Dowding/Dowding.cs
+++ ExtLibs/WebAPIs/Dowding/Dowding.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,30 +16,77 @@ namespace MissionPlanner.WebAPIs
 {
     public class Dowding
     {
-        private AuthenticationApi auth;
-        private AuthToken token;
+        private Configuration Configuration;
+        private string token;
         public string URL { get; set; } = "https://test.dowding.cuas.dds.mil/api/1.0";
         public string WS { get; set; } = "wss://test.dowding.cuas.dds.mil/ws";
 
+        public Dowding()
+        {
+            Configuration.Default.DefaultHeader["User-Agent"] =
+                "MissionPlanner " + Assembly.GetExecutingAssembly().GetName().Version;
+        }
+
         public async Task Auth(string email, string password)
         {
-            auth = new AuthenticationApi(URL);
+            Configuration = new Configuration(new ApiClient(URL));
 
-            token = await auth.AuthenticationLoginPostAsync(new LoginDto(email, password));
+            var auth = new AuthenticationApi(Configuration);
+
+            var tokenres = await auth.AuthenticationLoginPostAsync(new LoginDto(email, password));
+
+            token = tokenres?.Token;
+
+            Configuration.AddApiKey("Authorization", "Bearer " + tokenres?.Token);
         }
 
         public void SetToken(string customtoken)
         {
-            token = new AuthToken(customtoken);
+            Configuration = new Configuration(new ApiClient(URL));
+
+            token = customtoken;
+
+            Configuration.AddApiKey("Authorization", "Bearer " + customtoken);
         }
 
-        public async Task<List<VehicleTick>> GetVehicles()
+        public async Task<List<AgentTick>> GetAgents()
         {
-            var vehicle = new VehicleTickApi(URL);
-            var list = await vehicle.VehicleTickGetAsync();
+            var agent = new AgentTickApi(Configuration);
+            var list = await agent.AgentTickGetAsync();
             return list;
         }
 
+        public async Task<List<VehicleTick>> GetVehicle(string contactIds = null)
+        {
+            var vehicle = new VehicleTickApi(Configuration);
+            var list = await vehicle.VehicleTickGetAsync(contactIds);
+            return list;
+        }
+
+        //https://test.dowding.cuas.dds.mil/api/1.0/contact?min_ts=1619582433732&max_ts=1619654399999&includeThreats=false&thin=true
+        public async Task<List<Contact>> GetContact(decimal? offset = null, decimal? limit = null, bool? thin = null,
+            string format = null, string maxLon = null, string minLon = null, string maxLat = null,
+            string minLat = null, string maxTs = null, string minTs = null)
+        {
+            var contact = new ContactApi(Configuration);
+            var list = await contact.ContactGetAsync(offset, limit, thin, format, maxLon, minLon, maxLat, minLat, maxTs,
+                minTs);
+            return list;
+        }
+
+        public async Task<List<Zone>> GetZone()
+        {
+            var zone = new ZoneApi(Configuration);
+            var list = await zone.ZoneGetAsync();
+            return list;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="type">contacts/vehicle_ticks/operator_ticks/homepoints/events</param>
+        /// <returns></returns>
         public async Task<WebSocket> StartWS<T>(string type = "vehicle_ticks")
         {
             var ws = new WebSocket(WS);
@@ -50,7 +98,7 @@ namespace MissionPlanner.WebAPIs
                 {
                     headers = new
                     {
-                        authorization = "Bearer " + token?.Token
+                        authorization = "Bearer " + token
                     }
                 }
             });
